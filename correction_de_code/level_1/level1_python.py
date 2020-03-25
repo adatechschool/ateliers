520320b966e6c5aef384e3bd1694c9d58437c84e
jessyjones <alya@moov.mg>
2020-03-25 09:42:44 +0100

    Create level1_python.py

---
+++ correction_de_code/level_1/level1_python.py
@@ -0,0 +1,7 @@
+def find_longest(string):
+    spl = str.split(" ")
+    longest = 0
+    i=0
+    while (i > spl.length):
+    if (spl(i).length > longest): longest = spl[i].length
+    return longest
